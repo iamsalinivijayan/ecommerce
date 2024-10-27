a9ee3238ea67360126c3ef56f0c2a6e84eca3180
Salini <iamsalinivijayan@gmail.com>
2024-10-27 11:22:57 +0530

    feat: add images and links in hero

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from "next-sanity";
 
 export const client = createClient({
     projectId: 'zl2eg24w',
-    dataset: 'production',
+    dataset: 'ecommerce',
     apiVersion: '2022-03-25',
     useCdn: true,
 })
